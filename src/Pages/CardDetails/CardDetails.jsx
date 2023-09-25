d8381e415ea83c41df1e3e38deb9956d40807277
mehedi08cloud <mehedimurad08@gmail.com>
2023-09-26 02:00:00 +0600

    successfully done by local storage

--- src/Pages/CardDetails/CardDetails.jsx
+++ src/Pages/CardDetails/CardDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import swal from 'sweetalert';
 
 
 const CardDetails = () => {
@@ -17,7 +18,29 @@ const CardDetails = () => {
     // },[id, cards])
     const { image, title, category,category_bg,card_bg,desc,donation } = card || {};
 
-    
+    const handleDonation = () =>{
+
+        const addedDonatedAmount = [];
+        const donatedAmount = JSON.parse(localStorage.getItem('donation'))
+        if(!donatedAmount){
+            addedDonatedAmount.push(card);
+            localStorage.setItem('donation', JSON.stringify(addedDonatedAmount))
+            swal("Good job!", "You clicked the button!", "success"); 
+        }
+        else{
+
+            const isExist = donatedAmount.find((card) => card.id ===idInt);
+            if(!isExist){
+                addedDonatedAmount.push(...donatedAmount,card);
+            localStorage.setItem('donation', JSON.stringify(addedDonatedAmount))
+            swal("Good job!", "You clicked the button!", "success");  
+            }
+            else{
+                swal("Oops!", "You clicked the button!", "error"); 
+            }
+            
+        }
+    }
     return (
         <div>
             
@@ -28,7 +51,7 @@ const CardDetails = () => {
                 </div>
                 <div className='mt-[-40px] pl-14'>
                 
-                    <button>Donate ${donation}</button>
+                    <button onClick={handleDonation}>Donate ${donation}</button>
                 </div>
                 <div className="p-6 text-justify">
                     <h4 className="font-sans text-2xl font-semibold tracking-normal text-blue-gray-900">
